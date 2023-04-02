1debb2f28d524ccba0437ac3e7380e1495eb3711
moevius237 <arzmoevius@gmai.com>
2023-04-02 21:57:40 +0200

    Mi primer commit

---
+++ src/testsbasicos.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class testsbasicos {
+	
+	@Before
+	public void setUp() throws Exception{
+		
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
