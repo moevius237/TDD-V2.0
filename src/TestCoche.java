efb48f30a128b00f33d42b05cf1c061d1ae260ce
moevius237 <arzmoevius@gmai.com>
2023-04-02 22:19:07 +0200

    Todos los metodos cambiados con mi nombre

--- src/TestCoche.java
+++ src/TestCoche.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 
 public class TestCoche{
 	@Test
-	public void test_al_crear_coche_un_coche_su_velocidad_es_cero() {
+	public void test_al_crear_coche_un_coche_su_velocidad_es_cero_Pablo() {
 		Coche nuevoCoche = new Coche();
 		Assertions.assertEquals(0, nuevoCoche.velocidad);
 	}
